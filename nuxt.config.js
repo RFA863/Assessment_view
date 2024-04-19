da7e121d18fa3e15063506c62337156f8b6cac40
RFA863 <rafifajrul77@gmail.com>
2024-04-19 14:34:00 +0700

    feat: login page

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-cms',
+    title: 'Assessment',
     htmlAttrs: {
       lang: 'en'
     },
@@ -61,44 +61,44 @@ export default {
 
   auth: {
     strategies: {
-        local: {
-            token: {
-                property: 'token',
-                required: true,
-                type: 'Bearer'
-            },
-            user: {
-                property: 'user',
-                // autoFetch: true
-            },
-            endpoints: {
-                login: {
-                    url: '/api/admin/login',
-                    method: 'post'
-                },
-                logout: {
-                    url: '/api/admin/logout',
-                    method: 'post'
-                },
-                user: {
-                    url: '/api/admin/user',
-                    method: 'get'
-                }
-            }
+      local: {
+        token: {
+          property: 'token',
+          required: true,
+          type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: {
+            url: '/api/admin/login',
+            method: 'post'
+          },
+          logout: {
+            url: '/api/admin/logout',
+            method: 'post'
+          },
+          user: {
+            url: '/api/admin/user',
+            method: 'get'
+          }
         }
+      }
     },
     redirect: {
-        login: '/login',
-        logout: '/login',
-        callback: '/login',
-        home: '/admin/dashboard'
+      login: '/login',
+      logout: '/login',
+      callback: '/login',
+      home: '/admin/dashboard'
     }
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     //baseURL: 'https://cms-api.appdev.my.id'
-	baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'http://127.0.0.1:8000'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -116,5 +116,9 @@ export default {
     //     "@babel/transform-async-to-generator"
     //   ]
     // },
+  },
+
+  router: {
+    middleware: 'redirect'
   }
 }
---
+++ pages/assessment/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="position-relative max-vh-100">
+    <div
+      class="position-absolute z-1 d-flex justify-content-center align-items-center w-100 h-100"
+    >
+      <div
+        class="text-black p-5"
+        style="backdrop-filter: blur(30px); border-radius: 20px"
+      >
+        <h1 class="d-flex justify-content-center pb-3">Login</h1>
+        <form>
+          <table>
+            <tr class="border-bottom border-black">
+              <td class="py-2">Email</td>
+              <td>:</td>
+              <td>
+                <input
+                  type="email"
+                  class="bg-transparent border border-0"
+                  style="outline: none; border: none"
+                />
+              </td>
+            </tr>
+            <tr class="border-bottom border-black py-3">
+              <td class="py-2">Password</td>
+              <td>:</td>
+              <td>
+                <input
+                  type="password"
+                  class="bg-transparent border border-0"
+                  style="outline: none; border: none"
+                />
+              </td>
+            </tr>
+          </table>
+          <b-button variant="primary" type="submit" class="my-4 w-100"
+            >Login</b-button
+          >
+        </form>
+      </div>
+    </div>
+    <div class="vh-100">
+      <img
+        src="~/assets/img/background.jpg"
+        class="img-fluid object-fit-cover h-100 w-100"
+        alt="background"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+};
+</script>
